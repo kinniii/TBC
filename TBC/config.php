c18cd0136f31257d3d3884e881a1434f717cf466
Tyler <kinnigans@gmail.com>
2022-11-02 11:46:24 -0400

    remove config from git

--- TBC/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'tbcmetal_admin');
-    define('DB_PASS', 'allstar1010');
-    define('DB_NAME', 'tbcmetal_ReferenceProgram');
-    define('ROOT_URL', 'https://tbcmetal.com/index.html');
-?>
